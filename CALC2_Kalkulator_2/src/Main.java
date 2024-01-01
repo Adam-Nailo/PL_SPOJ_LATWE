fb22c2d7e71d33ec4a8f9dc3e2bd47a0334e1782
Adam <adam-seweryn@o2.pl>
2024-01-02 00:00:23 +0100

    initial commit

---
+++ CALC2_Kalkulator_2/src/Main.java
@@ -0,0 +1,44 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by Adam Seweryn
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> dane = new LinkedList<>();
+        while (scanner.hasNextLine()) {
+            dane.add(scanner.nextLine());
+        }
+        String[][] informacje = new String[dane.size()][3];
+        int[] memory = new int[10];
+        for (int i = 0; i < dane.size(); i++) {
+            String[] param = dane.get(i).split(" ");
+            informacje[i][0]=param[0];
+            informacje[i][1]=param[1];
+            informacje[i][2]=param[2];
+            switch (informacje[i][0].charAt(0)){
+                case ('z'):
+                    memory[Integer.parseInt(informacje[i][1])]=Integer.parseInt(informacje[i][2]);
+                    break;
+                case ('+'):
+                    System.out.println(memory[Integer.parseInt(informacje[i][1])]+memory[Integer.parseInt(informacje[i][2])]);
+                    break;
+                case ('-'):
+                    System.out.println(memory[Integer.parseInt(informacje[i][1])]-memory[Integer.parseInt(informacje[i][2])]);
+                    break;
+                case ('*'):
+                    System.out.println(memory[Integer.parseInt(informacje[i][1])]*memory[Integer.parseInt(informacje[i][2])]);
+                    break;
+                case ('/'):
+                    System.out.println(memory[Integer.parseInt(informacje[i][1])]/memory[Integer.parseInt(informacje[i][2])]);
+                    break;
+                case ('%'):
+                    System.out.println(memory[Integer.parseInt(informacje[i][1])]%memory[Integer.parseInt(informacje[i][2])]);
+                    break;
+            }
+        }
+    }
+}
