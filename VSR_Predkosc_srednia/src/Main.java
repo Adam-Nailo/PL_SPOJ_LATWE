dd94baa653da81228ebb46c5ea9676bf55c0d503
Adam <adam-seweryn@o2.pl>
2024-01-03 22:32:32 +0100

    version 2.0

--- VSR_Predkosc_srednia/src/Main.java
+++ VSR_Predkosc_srednia/src/Main.java
@@ -1,20 +1,50 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-/**
- * Created by Adam Seweryn
- */
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String x;
-        bufferedReader.readLine();
-        short a, b;
-        while ((x = bufferedReader.readLine()) != null) {
-            StringTokenizer dane = new StringTokenizer(x, " ");
-            System.out.println(2 * (a = Short.parseShort(dane.nextToken())) * (b = Short.parseShort(dane.nextToken())) / (a + b));
+class Parser {
+    byte[] buffer;
+
+    int offs = 0;
+
+    public Parser(int len) throws java.lang.Exception {
+        System.in.read(buffer = new byte[len]);
+    }
+
+    int readInt() {
+        int result = 0;
+
+        while (buffer[offs] <= ' ') {
+            ++offs;
         }
+
+        int c;
+
+        while ((c = buffer[offs++]) > ' ') {
+            result = (result << 3) + (result << 1) + (c - '0');
+        }
+
+        return result;
+    }
+}
+
+class Main {
+
+    public static void main(String[] args) throws java.lang.Exception {
+        Parser in = new Parser(10001 * 10);
+
+        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        int t = in.readInt();
+
+        while (t-- != 0) {
+            int v1 = in.readInt();
+
+            int v2 = in.readInt();
+
+            out.println(2 * v1 * v2 / (v1 + v2));
+        }
+
+        out.flush();
     }
 }
