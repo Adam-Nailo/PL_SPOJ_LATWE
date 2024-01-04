ec17403bbdedd5a4a2197299c2a7ee9273c19dc0
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-01-04 21:08:52 +0100

    version 2.0

--- MWPZ06A_Ciazowy_specjalista/src/Main.java
+++ MWPZ06A_Ciazowy_specjalista/src/Main.java
@@ -1,8 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /**
  * Created by Adam Seweryn
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        int x, y, z;
+        double dziecko;
+        for (int i = 0; i < n; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+            x = Integer.parseInt(stringTokenizer.nextToken());
+            y = Integer.parseInt(stringTokenizer.nextToken());
+            z = Integer.parseInt(stringTokenizer.nextToken());
+            dziecko = -12 * (double) (x + y - y * z) / (double) (z - 1);
+            System.out.println(Math.round(dziecko));
+        }
     }
 }
