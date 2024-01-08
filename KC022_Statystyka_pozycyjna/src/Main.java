07a9b8e648a9287fa1376236e8a901bd968285aa
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-01-08 19:42:02 +0100

    initial commit

---
+++ KC022_Statystyka_pozycyjna/src/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Adam Seweryn
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int liczba_wierszy = 0;
+        List<String> wiersze = new LinkedList<>();
+        while (bufferedReader.ready()) {
+            wiersze.add(bufferedReader.readLine());
+            liczba_wierszy++;
+        }
+        int[] pierwsze_znaki = new int[liczba_wierszy];
+        for (int i = 0; i < liczba_wierszy; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(wiersze.get(i));
+            pierwsze_znaki[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+        for (int i=0;i<liczba_wierszy;i++){
+            List<String> liczby = new LinkedList<>();
+            StringTokenizer stringTokenizer = new StringTokenizer(wiersze.get(i));
+            while (stringTokenizer.hasMoreElements()){
+                liczby.add(stringTokenizer.nextToken());
+            }
+            if ()
+        }
+    }
+}
